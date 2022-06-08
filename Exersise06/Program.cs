aa96a87bff0c2b1567b52a67d370e79b77b3e3eb
Aliaksei Poliak <polyackalexei@yandex.by>
2022-06-08 07:51:48 -0400

    Add Exersise08 even numbers

---
+++ Exersise06/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 6: Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+Console.WriteLine("Write first number");
+string numberA = Console.ReadLine();
+int A = Convert.ToInt32(numberA);
+
+int r = A % 2;
+
+if(r == 0)
+{
+    Console.WriteLine("This number is even");
+}
+else 
+{
+    Console.WriteLine("This number is odd");
+}
---
+++ Exersise08/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 8: Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.WriteLine("Write your number");
+string numberN = Console.ReadLine();
+int N = Convert.ToInt32(numberN);
+
+int i = 1;
+
+while(i < N)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
+
