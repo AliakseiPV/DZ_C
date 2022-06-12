3b3438ca1492a376bc7dc029f5c31e6ef7048902
Aliaksei Poliak <polyackalexei@yandex.by>
2022-06-12 04:12:54 -0400

    Exersise10 three-digit number

---
+++ Exersise10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на
+//вход трёхзначное число и на выходе показывает вторую
+//цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+Console.WriteLine("Enter a three-digit number");
+int A = int.Parse(Console.ReadLine());
+
+A = (A / 10) % 10;
+Console.WriteLine("The second digit of the nimber is: " + A); 
