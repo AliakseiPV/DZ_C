ca5dc68cfa82e58fe8897e87aeb6cf6b07e8354b
Nina <ninakaratk22@gmail.com>
2022-06-15 05:47:22 -0400

    Add Exersise21 3d Distance

---
+++ Exersise21/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на
+//вход координаты двух точек и находит расстояние между
+//ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Enter the coordinates (x,y,z) of the first point in 3d spase");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the coordinates (x,y,z) of the second point in 3d spase");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+double D = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+Console.WriteLine("The distance between two points is: " + Math.Round(D, 2));
+
+
