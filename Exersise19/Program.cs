d7808974f1c4c585be7ffc5a9e9f466100c45100
Nina <ninakaratk22@gmail.com>
2022-06-15 05:29:49 -0400

    Add Exersise19 Palindrom

---
+++ Exersise19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на
+//вход пятизначное число и проверяет, является ли оно
+//палиндромом.
+
+Console.WriteLine("Vvedite piatiznachnoe chislo");
+int A = int.Parse(Console.ReadLine());
+
+int a1 = A / 10000;
+int a2 = (A / 1000) % 10;
+int a4 = (A % 100) / 10;
+int a5 = (A % 10);
+
+if(a1 == a5 && a2 == a4)
+{
+    Console.WriteLine("Chislo " + A + " palindrom");
+}
+else{
+    Console.WriteLine("Chislo " + A + " ne palindrom");
+}
